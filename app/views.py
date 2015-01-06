6a3a0eacb480659131800c90a18add9092123bcc
Jelena Cvitanovic <cvitanovic.jelena@gmail.com>
2015-01-06 14:56:44 -0500

    Login form first draft

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -20,3 +21,10 @@ def index():
 							title = 'Home',
 							user = user,
 							posts = posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	return render_template('login.html',
+							title = 'Sign In',
+							form = form)
