3cbb1671423e449dd4872eb4b22961f90bb21de0
Jelena Cvitanovic <cvitanovic.jelena@gmail.com>
2015-01-12 14:45:23 -0500

    database changes for followers. db migrated successfully at last

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from app import db
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -9,7 +13,12 @@ class User(db.Model):
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime)
-   
+	followed = db.relationship('User', 
+								secondary=followers, 
+								primaryjoin=(followers.c.follower_id == id), 
+								secondaryjoin=(followers.c.followed_id == id), 
+								backref=db.backref('followers', lazy='dynamic'), 
+								lazy='dynamic')   
 
 	def is_authenticated(self):
 		return True
